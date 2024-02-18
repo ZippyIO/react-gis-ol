55696a07e82fb133070898dc8542f8ca6505bbd9
Nick <delta2888@gmail.com>
2024-02-19 10:16:04 +1100

    build: :heavy_plus_sign: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,38 @@
+import '~/styles/index.css';
+
+import { type Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#101012',
+        },
+        {
+          name: 'white',
+          value: '#ffffff',
+        },
+        {
+          name: 'black',
+          value: '#000000',
+        },
+      ],
+    },
+  },
+};
+
+export default preview;

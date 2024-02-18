97d568935e15e0e2ac8ca6846e61ca6ad7dc6f27
Nick <delta2888@gmail.com>
2024-02-19 10:33:57 +1100

    chore: :wrench: update eslint config setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,43 @@
-module.exports = {
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  plugins: ['react-refresh', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '!.storybook'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'end-of-line': 'off',
+    'no-unused-vars': 'off',
+    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
   },
 };
+
+module.exports = config;
